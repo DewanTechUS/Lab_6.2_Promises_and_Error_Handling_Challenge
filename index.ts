8315503ccaaf3960d7abd958931a2b9388cc37d4
Dewan Mahmud Rocky <rockyit10@gmail.com>
2025-11-18 00:42:27 -0500

    add error handling with catch and final message with finally

--- index.ts
+++ index.ts
@@ -4,21 +4,32 @@ import {
   fetchSalesReport
 } from "./apiSimulator";
 
-console.log("Starting fake API calls");
-// https://ps-lms.vercel.app/curriculum/se/413/lab-2#:~:text=Implement%20Error%20Handling%20Using%20Promises%3A
-//  Promise chain as per the instructions 
-fetchProductCatalog()
-.then((products) => {
-    console.log("Product catalog:", products);
-    return fetchProductReviews(products[0].id);
- })
+console.log("Starting fake API requests please wait.");
 
-.then((reviews) => {
+// Chained API calls with proper error handling
+// Implement Error Handling Using Promises:
+// https://ps-lms.vercel.app/curriculum/se/413/lab-2#:~:text=Implement%20Error%20Handling%20Using%20Promises%3A
+// follow the lesson // chaining the promises
+fetchProductCatalog()
+  .then((products) => {
+    console.log("Product catalogs:", products);
+    return fetchProductReviews(products[0].id);
+  })
+  .then((reviews) => {
     console.log("Product reviews:", reviews);
     return fetchSalesReport();
   
-})
 
-.then((salesReport) => {
-    console.log("Sales report:", salesReport);
+})
+  .then((salesReport) => {
+    console.log("Sales reports:", salesReport);
+  
+
+})
+  .catch((error) => {
+    console.error("Error:", error);
+  })
+
+  .finally(() => {
+    console.log("All fake API calls attempted");
   });
