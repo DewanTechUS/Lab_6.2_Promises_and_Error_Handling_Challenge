a0b0741ead46aac23e37cee5069ee386c951fbea
Dewan Mahmud Rocky <rockyit10@gmail.com>
2025-11-18 00:38:31 -0500

    complete Promise chain with product reviews and sales report

--- index.ts
+++ index.ts
@@ -1,9 +1,24 @@
-import { fetchProductCatalog, fetchProductReviews, fetchSalesReport } from "./apiSimulator";
+import {
+  fetchProductCatalog,
+  fetchProductReviews,
+  fetchSalesReport
+} from "./apiSimulator";
 
-console.log("Starting fake API requests Please wait.");
-
+console.log("Starting fake API calls");
+// https://ps-lms.vercel.app/curriculum/se/413/lab-2#:~:text=Implement%20Error%20Handling%20Using%20Promises%3A
+//  Promise chain as per the instructions 
 fetchProductCatalog()
-  .then((products) => {
+.then((products) => {
     console.log("Product catalog:", products);
     return fetchProductReviews(products[0].id);
+ })
+
+.then((reviews) => {
+    console.log("Product reviews:", reviews);
+    return fetchSalesReport();
+  
+})
+
+.then((salesReport) => {
+    console.log("Sales report:", salesReport);
   });
